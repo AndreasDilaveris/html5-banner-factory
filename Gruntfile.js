4268513689515e7604f5c3de23ea656fdfb4c906
AndreasDilaveris <control@turnoffthe.tv>
2014-06-25 19:38:12 +0100

    added structural files -grunt

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports  = function(grunt){
+	grunt.initConfig({
+
+	});
+
+	/* */
+	
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-sass");
+	grunt.loadNpmTasks("grunt-contrib-watch");				
+}
